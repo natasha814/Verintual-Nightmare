c8f955c109418dc969f147f77efba1434fe72ccc
“Zeta <chazapi.zafeiria@gmail.com>
2024-10-12 13:06:01 +0100

    World Class

--- Location/Forest.java
+++ Location/Forest.java
@@ -1,6 +1,6 @@
 package Location;
 
-public class Forest extends Location{
+public class Forest extends Location {
 
     private String item;
 
@@ -11,17 +11,17 @@ public class Forest extends Location{
 
     @Override
     public void enter(Player player) {
-        System.out.println("You enter the "+getName()+": "+getDescription());
+        System.out.println("You enter the " + getName() + ": " + getDescription());
         if (item != null) {
             String a_or_an = "a";
             if (checkFirstLetterVowel(item)) {
                 a_or_an = "an";
             }
-            System.out.println("You see a "+item+" here.");
+            System.out.println("You see a " + item + " here.");
         }
         // will add code to pick up item etc
     }
-    
+
     private boolean checkFirstLetterVowel(String str) {
         char lower_char = Character.toLowerCase(str.charAt(0));
         if (lower_char == 'a' || lower_char == 'e' || lower_char == 'i' || lower_char == 'o' || lower_char == 'u') {
--- World.java
+++ World.java
@@ -1,6 +1,47 @@
-public class World{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    public World() {
+import Location.Forest;
+
+public class World {
+    private Player player;
+    private List<Locations> locations;
+
+    public World(Player player) {
+        this.player = player;
+        this.location = new ArrayList<>();
+        initializeLocations();
+
+    }
+
+    private void initializelocations() {
+        Location clownHouse = new Clown_House("Clown House", "wkufjidhfhwf");
+        Location forest = new Forest("Forest", "wkufjidhfhwf", "axe");
+    }
+
+    locations.add(clownHouse);locations.add(forest);
+
+    // if user chooses Clown House
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);  
+        System.out.println("Enter username");
+
+        String userName = userInput.nextLine();  
+        System.out.println("Username is: " + userName);  
+
+        System.out.println("Time for you to choose your fate. Do not be too quick to choose, press 'I' for more information");
+        
+        if 
+        
+        
+        System.out.println("Ready? Enter 'C' for Clown House or 'F' for Forest");
+        
+
+        player.moveTo(choice);
+
         
     }
+
 }
