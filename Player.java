5faf1818ba3e3ef40f5a0997ac2bb7f64c5a2d7b
Chloe Liu <2739541l@student.gla.ac.uk>
2024-10-12 13:03:32 +0100

    Player

--- Player.java
+++ Player.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    
+    private String name;
+    private Location currentLocation;
+    private List<Item> inventory;
+
+    public Player(String name, Location currentLocation){
+        this.name = name;
+        this.currentLocation = currentLocation;
+        this.inventory = new ArrayList<Item>();
+    }
+
+    public void moveTo(Location newLocation){
+        currentLocation = newLocation;
+        cuurentLocation.enter(newLocation);
+    }
+
+    public void addItem(Item item){
+        inventory.add(item);
+        System.out.println("You've picked up: "  + item);
+    }
+
 }
